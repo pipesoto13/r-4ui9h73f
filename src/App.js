dedebc76e5daf71d44c70c3236441caaa3c36473
Felipe Soto C <pipesoto13@gmail.com>
2020-10-21 11:52:35 -0500

    solved

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { content: '' };
+
+    this.copyText = this.copyText.bind(this);
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.copyText} />
+    <p className="repeater">{this.state.content}</p>
       </div>
     );
+  }
+
+  copyText(event) {
+    let text = event.target.value;
+    this.setState({ content: text });
   }
 }
 
